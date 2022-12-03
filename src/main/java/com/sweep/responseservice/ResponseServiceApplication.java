743f20c915c8b2a5149ffff0fc832bc2227a3ed2
banggeunho <panggeunho@gmail.com>
2022-12-03 17:25:47 +0900

    timezone 설정 추가

--- src/main/java/com/sweep/responseservice/ResponseServiceApplication.java
+++ src/main/java/com/sweep/responseservice/ResponseServiceApplication.java
@@ -8,12 +8,22 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
+import javax.annotation.PostConstruct;
+import java.util.Date;
+import java.util.TimeZone;
+
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @EnableDiscoveryClient
 @EnableFeignClients
 @EnableMongoRepositories(basePackages = "com.sweep.responseservice.domain")
 public class ResponseServiceApplication {
 
+    @PostConstruct
+    public void started() {
+        System.out.println("현재시각 : " + new Date());
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+        System.out.println("현재시각 : " + new Date());
+    }
     public static void main(String[] args) {
         SpringApplication.run(ResponseServiceApplication.class, args);
     }
